a70846a333845cc6b77b3c2f2089729948caddcd
qcloud <ubuntu@localhost.localdomain>
2023-11-17 17:04:37 +0800

    upate game.js

--- server.js
+++ server.js
@@ -1,38 +1,63 @@
 const WebSocket = require('ws');
 
-// 创建WebSocket服务器监听在8080端口
+// 创建一个WebSocket服务器的实例
 const wss = new WebSocket.Server({ port: 8080 });
 
-// 存储所有的客户端连接
-const clients = [];
+// 用于存储所有玩家状态的对象
+const players = {};
 
-// 广播给所有客户端的函数
-function broadcast(data) {
-  for (const client of clients) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
+// 生成随机颜色的函数
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-  }
+    return color;
 }
 
-wss.on('connection', function connection(ws) {
-  // 添加新连接的客户端
-  clients.push(ws);
+// 广播所有玩家状态的函数
+function broadcast(data) {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+}
 
-  // 当服务器收到来自客户端的消息
-  ws.on('message', function incoming(message) {
-    // 将收到的消息广播给所有客户端
-    broadcast(message);
-  });
+// 当有玩家连接到服务器时
+wss.on('connection', ws => {
+    // 为新玩家分配一个唯一的ID和初始状态
+    const currentPlayerID = Date.now().toString();
+    players[currentPlayerID] = { x: 50, y: 50, radius: 10, color: getRandomColor() };
 
-  // 当客户端断开连接
-  ws.on('close', function close() {
-    // 从客户端列表中移除
-    const index = clients.indexOf(ws);
-    if (index > -1) {
-      clients.splice(index, 1);
-    }
-  });
+    // 通知新玩家他们的ID和颜色，并广播所有玩家状态
+    ws.send(JSON.stringify({ type: 'init', id: currentPlayerID, color: players[currentPlayerID].color }));
+    broadcast(JSON.stringify({ type: 'update', players: players }));
+
+    // 当从客户端接收到消息时
+    ws.on('message', message => {
+        const msg = JSON.parse(message);
+
+        // 更新发送消息的玩家的位置
+        if(players[msg.id]) {
+            players[msg.id].x = msg.x;
+            players[msg.id].y = msg.y;
+        }
+
+        // 广播更新后的所有玩家状态
+        broadcast(JSON.stringify({ type: 'update', players: players }));
+    });
+
+    // 当玩家断开连接时
+    ws.on('close', () => {
+        // 从玩家状态中移除
+        delete players[currentPlayerID];
+
+        // 广播更新后的所有玩家状态
+        broadcast(JSON.stringify({ type: 'update', players: players }));
+    });
 });
 
 console.log('WebSocket server is running on port 8080');
+
--- src/game.js
+++ src/game.js
@@ -2,8 +2,12 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// 设置玩家的初始状态
+// 存储所有玩家的对象
+var players = {};
+
+// 设置本地玩家的初始状态
 const player = {
+  id: null,
   x: canvas.width / 2,
   y: canvas.height / 2,
   radius: 20,
@@ -13,37 +17,64 @@ const player = {
   dy: 0
 };
 
-// 画圆形的函数
-function drawPlayer() {
-  ctx.beginPath();
-  ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
-  ctx.fillStyle = player.color;
-  ctx.fill();
-  ctx.closePath();
+// 创建WebSocket连接
+const ws = new WebSocket('ws://YOUR_SERVER_IP:8080');
+
+// WebSocket事件监听
+ws.onopen = function() {
+  console.log('Connected to the server');
+};
+
+ws.onmessage = function(event) {
+  const message = JSON.parse(event.data);
+  if (message.type === 'update') {
+    players = message.players;
+  } else if (message.type === 'init') {
+    player.id = message.id;
+    player.color = message.color; // 服务器指定颜色
+  }
+};
+
+// 发送本地玩家的状态到服务器
+function sendPlayerUpdate() {
+  if (player.id !== null) {
+    ws.send(JSON.stringify({
+      type: 'update',
+      id: player.id,
+      x: player.x,
+      y: player.y
+    }));
+  }
 }
 
-// 更新玩家位置的函数
+// 更新本地玩家位置的函数
 function updatePlayerPosition() {
   player.x += player.dx;
   player.y += player.dy;
 
   // 检测边界
-  if (player.x + player.radius > canvas.width) {
-    player.x = canvas.width - player.radius;
-  } else if (player.x - player.radius < 0) {
-    player.x = player.radius;
-  }
+  player.x = Math.max(player.radius, Math.min(canvas.width - player.radius, player.x));
+  player.y = Math.max(player.radius, Math.min(canvas.height - player.radius, player.y));
 
-  if (player.y + player.radius > canvas.height) {
-    player.y = canvas.height - player.radius;
-  } else if (player.y - player.radius < 0) {
-    player.y = player.radius;
-  }
+  // 发送位置更新
+  sendPlayerUpdate();
 }
 
 // 清除Canvas的函数
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// 绘制所有玩家
+function drawPlayers() {
+  Object.keys(players).forEach(function(id) {
+    const otherPlayer = players[id];
+    ctx.beginPath();
+    ctx.arc(otherPlayer.x, otherPlayer.y, otherPlayer.radius, 0, Math.PI * 2);
+    ctx.fillStyle = otherPlayer.color;
+    ctx.fill();
+    ctx.closePath();
+  });
 }
 
 // 键盘事件监听器
@@ -60,15 +91,9 @@ function keyDownHandler(e) {
 }
 
 function keyUpHandler(e) {
-  if (
-    e.key === 'Right' || e.key === 'ArrowRight' ||
-    e.key === 'Left' || e.key === 'ArrowLeft'
-  ) {
+  if (['Right', 'ArrowRight', 'Left', 'ArrowLeft'].includes(e.key)) {
     player.dx = 0;
-  } else if (
-    e.key === 'Up' || e.key === 'ArrowUp' ||
-    e.key === 'Down' || e.key === 'ArrowDown'
-  ) {
+  } else if (['Up', 'ArrowUp', 'Down', 'ArrowDown'].includes(e.key)) {
     player.dy = 0;
   }
 }
@@ -80,11 +105,11 @@ document.addEventListener('keyup', keyUpHandler);
 // 游戏主函数
 function game() {
   clearCanvas();
-  drawPlayer();
+  drawPlayers(); // 现在这个函数负责绘制所有玩家
   updatePlayerPosition();
-
   requestAnimationFrame(game);
 }
 
 // 开始游戏
 game();
+
